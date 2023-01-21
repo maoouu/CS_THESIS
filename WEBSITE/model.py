63477f8daf53f76fc56826bf6f596d31185e2262
maoouu <dale.reyes@outlook.ph>
2023-01-21 16:55:18 +0800

    refactored model.py

--- WEBSITE/model.py
+++ WEBSITE/model.py
@@ -2,21 +2,35 @@
 import pandas as pd
 import pickle
 
+from config import DATASET_FOLDER
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 
-dataset = pd.read_csv('data/features_3_sec.csv')
-dataset = dataset.iloc[:, 2:]
-y = dataset['label'] # target variables
-X = dataset.loc[:, dataset.columns != 'label'] # feature variables
 
-# Preprocessing
-scaler = MinMaxScaler()
-X_scaled = scaler.fit_transform(X)
+class KNeighborsClassifierModel:
+    def __init__(self, n_neighbors=5, weights='distance', scaler=None) -> None:
+        self.n_neighbors = n_neighbors
+        self.weights = weights
+        self.scaler = scaler
+        self.model = KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
 
-# Model
-k_neighbors_model = KNeighborsClassifier(n_neighbors=3, weights='distance')
-k_neighbors_model.fit(X_scaled, y)
 
-# Save Model to disk
-pickle.dump(k_neighbors_model, open('model.pkl', 'wb'))
+    def fit(self, X, y):
+        if self.scaler:
+            X = self.scaler.fit_transform(X)
+        self.model.fit(X, y)
+    
+
+    def save(self, file_path):
+        pickle.dump(self.model, open(file_path, 'wb'))
+    
+
+
+if __name__ == '__main__':
+    dataset = pd.read_csv(DATASET_FOLDER).iloc[:, 2:]
+    y = dataset['label'] # target variable
+    X = dataset.loc[:, dataset.columns != 'label'] # feature variable
+    scaler = MinMaxScaler()
+    model = KNeighborsClassifierModel(scaler=scaler)
+    model.fit(X, y)
+    model.save('model.pkl')

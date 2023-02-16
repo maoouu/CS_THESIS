e98b26cefe222b59fda081851c83552deae08aeb
maoouu <dale.reyes@outlook.ph>
2023-02-16 15:52:41 +0800

    added split_audio_chunks() function

--- WEBSITE/utils/file_handling.py
+++ WEBSITE/utils/file_handling.py
@@ -1,4 +1,5 @@
 # file_handling.py
+import librosa
 import os
 from config import ALLOWED_EXTENSIONS
 from pydub import AudioSegment
@@ -39,3 +40,24 @@ def convert_mp3_to_wav(filepath) -> str:
     audio.export(output, format='wav')
     os.remove(filepath)
     return output
+
+
+def split_audio_chunks(filepath, window_size=3):
+    """
+    Takes audio file and splits it into chunks.
+    It returns a list of chunks that contain the audio file.
+
+    Parameters:
+    filepath (str): The path to the audio file
+    window_size (int): The size of the chunks in seconds. Defaults to 3.
+
+    Returns:
+    samples (list): A list of audio chunks.
+    """
+    audio, sampling_rate = librosa.load(filepath)
+    split_length = sampling_rate * window_size
+    chunks = []
+    for start in range(0, len(audio), split_length):
+        end = start + split_length
+        chunks.append(audio[start:end])
+    return chunks

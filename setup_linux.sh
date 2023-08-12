f6e80fc548920b86148b82d614742783d7a2059b
maoouu <dale.reyes@outlook.ph>
2023-08-12 11:09:05 +0800

    updated setup script

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,6 +2,8 @@ if [ -d "WEBSITE/.env" ]; then
     echo "A python virtual environment has already been created."
 else
     cd WEBSITE/
+    apt install python3-pip
+    apt install python3.10-venv
     python3 -m venv .env
     source .env/bin/activate
     python3 -m pip install poetry

8df3cc90cbe2bb5fa388b2a25a4ea40570b1a17a
maoouu <dale.reyes@outlook.ph>
2023-08-12 11:31:11 +0800

    added ffmpeg to setup; added uploads/ folder

--- setup_linux.sh
+++ setup_linux.sh
@@ -2,12 +2,14 @@ if [ -d "WEBSITE/.env" ]; then
     echo "A python virtual environment has already been created."
 else
     cd WEBSITE/
-    apt install python3-pip
-    apt install python3.10-venv
+    sudo apt install python3-pip
+    sudo apt install python3.10-venv
+    sudo apt install ffmpeg
     python3 -m venv .env
     source .env/bin/activate
     python3 -m pip install poetry
     poetry install
-
+    python3 model.py
     echo "Successfully created python virtual environment."
+    echo "Use the command 'python3 app.py' to run the website."
 fi

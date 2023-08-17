9894077e17cc87bd4c3a30f27920fb7811bc8cf6
maoouu <dale.reyes@outlook.ph>
2023-08-18 02:08:21 +0800

    unified youtube link and upload form submission

--- WEBSITE/app.py
+++ WEBSITE/app.py
@@ -10,7 +10,7 @@ from features import extract_features
 from flask import Flask, request, render_template, abort, redirect, url_for
 from werkzeug.utils import secure_filename
 from utils.file_handling import file_is_allowed, convert_mp3_to_wav, split_audio_chunks
-from youtube_converter import download_youtube_audio, convert_to_wav, classify_youtube_audio
+from utils.youtube_converter import download_youtube_audio, get_title
 
 # Initialize Flask App
 app = Flask(__name__)
@@ -25,23 +25,30 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    input_file = request.form.get('input')
-    if input_file.startswith("https://www.youtube.com/"):
-        audio_file = download_youtube_audio(input_file)
-        wav_file = convert_to_wav(audio_file)
-    else:
+    if request.method == 'POST':
+        input_file = request.form.get('input')
+
+        if input_file and input_file.startswith("https://www.youtube.com/"):
+            # Download and convert YouTube audio to WAV
+            audio_file = download_youtube_audio(input_file)
+            wav_file = convert_mp3_to_wav(audio_file)
+            return redirect(url_for('.classify', file=wav_file, filename='Unknown'))
+
         uploaded_file = request.files['file']
         filename = secure_filename(uploaded_file.filename)
         file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
         if filename != '':
             if not file_is_allowed(filename):
                 abort(400)
             uploaded_file.save(file)
             wav_file = convert_mp3_to_wav(file)
-    
-    return redirect(url_for('.classify', file=wav_file, filename='Unknown'))
+            return redirect(url_for('.classify', file=wav_file, filename=filename[:-4]))
+
+    return render_template('index.html')  # Render the upload form if it's a GET request
+
 
 
 @app.route('/preprocess', methods=['GET', 'POST'])
@@ -70,7 +77,7 @@ def classify():
         feature = pd.DataFrame(feature, index=[0])
         predictions.append(model.predict(feature)[0])
     result = Counter(predictions).most_common(1)[0][0]
-    return render_template('index.html', result=str(result), filename=str(filename)[:-4])
+    return render_template('index.html', result=str(result), filename=str(filename))
 
 if __name__ == '__main__':
     app.run(debug=True)

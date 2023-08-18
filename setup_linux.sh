f924d19861bd4a55084122ac03e5b1dcae20e9f9
maoouu <dale.reyes@outlook.ph>
2023-08-18 09:03:15 +0800

    updated setup script for linux

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,12 +4,12 @@ else
     cd WEBSITE/
     sudo apt install python3-pip
     sudo apt install python3.10-venv
-    sudo apt install ffmpeg
-    python3 -m venv .env
-    source .env/bin/activate
-    python3 -m pip install poetry
-    poetry install
-    python3 model.py
     echo "Successfully created python virtual environment."
-    echo "Use the command 'python3 app.py' to run the website."
+    python3 -m venv .env
 fi
+cd WEBSITE/
+source .env/bin/activate
+python3 -m pip install poetry
+poetry install
+python3 model.py
+echo "Use the command 'python3 app.py' to run the website."
